32f0f19da1a2f138673c16a43bfa7004da662221
Lev Durov <gerian109@yandex.ru>
2023-12-11 17:57:28 +0300

    done без тестов и без isSorted

---
+++ src/ArrayList_LevDurov.java
@@ -0,0 +1,213 @@
+import javax.swing.*;
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * Реализация собственного динамического списка (аналог ArrayList, не потокобезопасный).
+ * @param <E> Тип элементов списка.
+ * @author Лев Дуров
+ * @see IntensiveList
+ */
+public class ArrayList_LevDurov<E> implements IntensiveList<E> {
+
+    /**
+     * Емкость списка по умолчанию
+     */
+    final static private int DEFAULT_CAPACITY = 10;
+    /**
+     * Массив, в котором хранятся элементы списка.
+     */
+    private Object[] array;
+    /**
+     * Количество элементов в списке.
+     */
+    private int size;
+
+    /**
+     * Создает пустой список с емкостью по умолчанию.
+     */
+    public ArrayList_LevDurov() {
+        size = 0;
+        this.array = new Object[DEFAULT_CAPACITY];
+    }
+
+    /**
+     * Создает пустой список с указанной емкостью.
+     * @param capacity Емкость списка.
+     */
+    public ArrayList_LevDurov(int capacity) {
+        size = 0;
+        this.array = new Object[capacity];
+    }
+
+    /**
+     * @return Количество элементов в списке.
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Добавляет элемент в конец списка.
+     * @param element Элемент для добавления в список.
+     */
+    @Override
+    public void add(E element) {
+        if (size == array.length)
+            grow();
+
+        array[size] = element;
+        size++;
+    }
+
+    /**
+     * Увеличивает емкость списка.
+     */
+    private void grow() {
+        int newCapacity = array.length * 2;
+        Object[] newArray = new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newArray[i] = array[i];
+        }
+        array = newArray;
+    }
+
+    /**
+     * Добавляет элемент в список на определенную позицию.
+     * @param index Индекс для вставки элемента.
+     * @param element Элемент для добавления в список.
+     */
+    @Override
+    public void add(int index, E element) {
+        if (size == array.length)
+            grow();
+        System.arraycopy(array, index, array, index + 1, size - index);
+        array[index] = element;
+        size++;
+    }
+
+    /**
+     * Возвращает элемент списка с заданным индексом.
+     * @param index Индекс элемента.
+     * @return Элемент списка.
+     */
+    @Override
+    public E get(int index) {
+        return (E) array[index];
+    }
+
+    /**
+     * Устанавливает значение элементу списка с заданным индексом.
+     * @param index Индекс списка.
+     * @param element Новый элемент для вставки.
+     * @return Возвращает старое значение ячейки.
+     */
+    @Override
+    public E set(int index, E element) {
+        E oldElem = (E) array[index];
+        array[index] = element;
+        return oldElem;
+    }
+
+    /**
+     * Удаляет элемент с заданным индексом из списка.
+     *
+     * @param index Индекс удаляемого элемента.
+     * @return Возвращает удаленный элемент.
+     */
+    @Override
+    public E remove(int index) {
+        E remElem = (E) array[index];
+        size--;
+        System.arraycopy(array, index + 1, array, index, size - index);
+        array[size] = null;
+
+        return remElem;
+    }
+
+    /**
+     * Удаляет все элементы из списка.
+     */
+    @Override
+    public void clear() {
+        array = new Object[DEFAULT_CAPACITY];
+    }
+
+    /**
+     * Сортирует список по возрастанию элементов, используя алгоритм быстрой сортировки.
+     * @param comparator Содержит функцию сравнения элементов коллекции.
+     */
+    @Override
+    public void quickSort(Comparator<E> comparator) {
+        quickSortRecurs(comparator, 0, array.length - 1);
+    }
+
+    private void quickSortRecurs(Comparator<E> comparator, int begin, int end) { // 2 3 4 1 6 5 7 -
+        if (begin < end) {
+            int mid = (end - begin) / 2 + begin;
+            E pivot = (E) array[mid];
+
+            for (int i = begin; i < mid; i++) {
+                if (comparator.compare((E) array[i], pivot) > 0) {
+                    Object o = array[i];
+                    for (int j = i; j < mid; j++) {
+                        array[j] = array[j + 1];
+                    }
+                    array[mid] = o;
+                    mid--;
+                }
+            }
+
+            for (int i = (end - begin) / 2 + begin + 1; i <= end; i++) {
+                if (comparator.compare((E) array[i], pivot) <= 0) {
+                    Object o = array[i];
+                    for (int j = i; j > mid; j--) {
+                        array[j] = array[j - 1];
+                    }
+                    array[i] = o;
+                    mid++;
+                }
+            }
+
+            quickSortRecurs(comparator, begin, mid - 1);
+            quickSortRecurs(comparator, mid + 1, end);
+        }
+    }
+
+    /**
+     * Проверяет, отсортирован ли список.
+     * @return Если список отсортирован по возрастанию возвращает true, иначе - false.
+     */
+    @Override
+    public boolean isSorted() {
+        if (size < 2)
+            return true;
+
+        return false;
+    }
+
+    /**
+     * Обрезает список до заданного размера, если текуцщий размер списка больше заданного.
+     *
+     * @param size Максимальный размер списка после обрезания.
+     */
+    @Override
+    public void split(int size) {
+        if (array.length > size) {
+            Object[] newArray = new Object[size];
+            for (int i = 0; i < size; i++) {
+                newArray[i] = array[i];
+            }
+            array = newArray;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "ArrayList_LevDurov{" +
+                "array=" + Arrays.toString(array) +
+                ", size=" + size +
+                '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1243");
+        IntensiveList<Integer> intList = new ArrayList_LevDurov<>();
+        intList.add(1);
+        intList.add(2);
+        intList.add(3);
+        intList.add(1);
+        intList.add(2);
+        intList.add(3);
+        intList.add(1);
+        intList.add(2);
+        intList.add(3);
+        intList.add(1);
+        intList.add(2);
+        intList.add(3);
+
+        intList.add(4, 999);
+        intList.set(8, 777);
+
+        System.out.println(intList.toString());
     }
 }
