50b3b131fe9113c6d40e73e5a61f8ca3a38fa6da
zheka4ka17 <114424499+zheka4ka17@users.noreply.github.com>
2023-12-18 14:06:31 +0100

    create task2

---
+++ IntensiveTest_YauheniDziamyanau.java
@@ -0,0 +1,15 @@
+package com.dziamyanau.task2;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Анотация создана для сканирования методов в пакете
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface IntensiveTest_YauheniDziamyanau {
+}
