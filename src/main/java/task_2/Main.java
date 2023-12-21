57f615367e65a2f93c9a4e5689d0e990365d8fe5
JYaromich <jane19937788@gmail.com>
2023-12-21 17:24:23 +0300

    task_2

---
+++ src/main/java/task_2/Main.java
@@ -0,0 +1,8 @@
+package task_2;
+
+public class Main {
+    public static void main(String[] args) {
+        TestRunner_AnastasiaYaromich testRunner = new TestRunner_AnastasiaYaromich("task_2");
+        testRunner.run();
+    }
+}
